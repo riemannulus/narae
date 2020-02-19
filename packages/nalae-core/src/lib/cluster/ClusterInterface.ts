32a61ceb888e9ebf089a82b6a648099ada1042b4
Lee Suho <riemannulus@hitagi.moe>
2020-02-20 01:06:47 +0900

    feat: return buf/path instead boolean

--- packages/nalae-core/src/lib/cluster/ClusterInterface.ts
+++ packages/nalae-core/src/lib/cluster/ClusterInterface.ts
@@ -1,6 +1,7 @@
 export default interface ClusterInterface {
-  write(filepath: string): boolean
-  read(filepath: string): boolean
+  write(buf: Buffer): string
+  read(filepath: string): Buffer
+  delete(filepath: string): boolean
   getAvaliableStorageList(): Array<string>
   addStorage(storageName: string, key: object): boolean
 }
--- packages/nalae-core/src/lib/cluster/NalaeCluster.ts
+++ packages/nalae-core/src/lib/cluster/NalaeCluster.ts
@@ -1,17 +1,27 @@
-import { IStorage } from '../storage'
+import { IStorage, StorageInterface } from '../storage'
 import ClusterInterface from './ClusterInterface'
 
 export default class NalaeCluster implements ClusterInterface {
-  write(filepath: string): boolean {
+  private storageList: Array<StorageInterface>;
+
+  constructor(){
+    //TODO: 
+  }
+
+  write(buf: Buffer): string {
     throw new Error("Method not implemented.");
   }
-  read(filepath: string): boolean {
+  read(filepath: string): Buffer {
+    throw new Error("Method not implemented.");
+  }
+  delete(filepath: string): boolean {
     throw new Error("Method not implemented.");
   }
   getAvaliableStorageList(): string[] {
     return IStorage.GetImplementations().map(x=>x.name);
   }
   addStorage(storageName: string, key: object): boolean {
+    
     throw new Error("Method not implemented.");
   }
 
