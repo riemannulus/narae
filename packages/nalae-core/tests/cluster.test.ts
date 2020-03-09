3ea0d9e9090754af5ee82a6b7d4f2b6c6456a7e9
Lee Suho <riemannulus@hitagi.moe>
2020-03-10 02:22:20 +0900

    test: cluster.test.ts

--- packages/nalae-core/tests/cluster.test.ts
+++ packages/nalae-core/tests/cluster.test.ts
@@ -1,20 +1,27 @@
 import { describe, it } from 'mocha'
+import { expect } from 'chai'
 import { NalaeCluster } from '../src/lib/cluster'
 import {MegaStorage, StorageInterface} from "../src/lib/storage";
 import {File} from "../src/lib/model/file";
+import {fileRepository} from "../src/lib/repository/FileRepository";
 
 
-let storageList: StorageInterface[];
+let storageList: Array<StorageInterface>;
 describe('cluster', function() {
   before(function() {
+    storageList = Array<StorageInterface>();
     storageList.push(new MegaStorage({userId: 'test', passwd: 'test2'}))
   });
 
   describe('write', function() {
     it('should be write file success', function () {
       let cluster = new NalaeCluster(storageList, 500);
-      let file: File = new File(Buffer.from('*'.repeat(4096)), 'test.txt');
+      let file: File = new File(Buffer.from('*'.repeat(4096)), 'test2.txt');
       cluster.write(file);
+
+      let metadata = fileRepository.findBy(file.metadata.name);
+
+      expect(file.metadata).to.equal(metadata);
     });
   });
 
